35146420ba9121709ba60b93f3ac1397bb155907
Natnael772 <natnaeldeyas0@gmail.com>
2023-08-31 19:58:25 +0300

    Injectable service

--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -3,4 +3,11 @@ import { Injectable } from '@nestjs/common';
 //Doesn't depend upon any thing inside the project
 
 @Injectable()
-export class PowerService {}
+export class PowerService {
+  //amount of power that other services is requesting to be supplied
+
+  //supply watts that some other services need to work
+  supplyPower(watts: number) {
+    console.log(`Supplying ${watts} worth of power`);
+  }
+}
