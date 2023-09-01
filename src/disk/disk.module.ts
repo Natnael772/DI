8a7c1ab53cb798ae3a33edd719d890ad8bd6071d
Natnael772 <natnaeldeyas0@gmail.com>
2023-09-01 20:41:04 +0300

    Disk module

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
-
+import { PowerModule } from 'src/power/power.module';
 @Module({
-  providers: [DiskService]
+  imports: [PowerModule],
+  providers: [DiskService],
+  exports: [DiskService],
 })
 export class DiskModule {}
